60b1b00766f46db013de137ee2cfe670f4c630b2
Victoria <mariavictoriafalmeida@gmail.com>
2023-03-02 23:04:29 -0300

    add 'do while'

--- aula39/script.js
+++ aula39/script.js
@@ -7,6 +7,13 @@ while (rand !== 10) {
     console.log(rand);
 }
 
+/*
+do {
+    rand = random(min, max);
+    console.log(rand);
+} while (rand !== 10);
+*/
+
 function random(min, max){
     const r = Math.random() * (max - min) + min;
     return Math.floor(r);

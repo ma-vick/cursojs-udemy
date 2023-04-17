7684191e32cf1337c61b58e92a5d9eaf17e29097
Victória Almeida <mariavictoriafalmeida@gmail.com>
2023-04-16 22:15:20 -0300

    aula62

---
+++ aula62/script.js
@@ -0,0 +1,11 @@
+const nomes = ['Laís', 'Breno', 'Gregory', 'William', 'Lena'];
+const novo = nomes.slice(0, -1);
+console.log(novo);
+
+const nome = 'Victória Almeida';
+const nominhos = nome.split(' ');
+console.log(nominhos);
+
+const nomeSeparado = ['Victória', 'Almeida'];
+const nomeJunto = nomeSeparado.join(' ');
+console.log(nomeJunto);
